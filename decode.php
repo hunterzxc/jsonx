2a289bd5dffb4168a718ef6f2158d2667da0abc3
hunterzxc <60175173+hunterzxc@users.noreply.github.com>
2020-02-12 15:13:17 +0800

    Update decode.php

--- decode.php
+++ decode.php
@@ -5,10 +5,10 @@ $db_username = 'admin';
 $db_password = 'root1234';
 $db_hostname = 'dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com';
 $db_port = '3306';
-$db_name = 'db_1822002';
+$db_name = 'db_1821982';
 
 $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
-$q = "SELECT * FROM Erp";
+$q = "SELECT * FROM hunter";
 $r = mysqli_query($conn, $q);
 
 while ($item = mysqli_fetch_object($r)) {
@@ -16,9 +16,9 @@ while ($item = mysqli_fetch_object($r)) {
 	$dec = json_decode($json, true);
 	
 	echo "<ul>";
-	echo "<li>". $dec["Name"]." </li>";
-	echo "<li>". $dec["Age"] ."</li>";
-	echo "<li>". $dec["Gender"] ."</li>";
+	echo "<li>". $dec["id"]." </li>";
+	echo "<li>". $dec["username"] ."</li>";
+	echo "<li>". $dec["password"] ."</li>";
 	echo "</ul>";
 	echo "</ul>";
 }
